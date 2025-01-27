89a3cb937865e847b440a3c3739bfc4efb693370
Joyboy-lucky <s77812636@gmail.com>
2025-01-27 20:20:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const formData = new FormData(event.target);
+    const params = new URLSearchParams(formData);
+  
+    fetch("https://script.google.com/macros/s/AKfycbytTOnXNQNMgB3ZyvfdoKgkcuvl_YntAM5FkDi7jFQN_K3BknTnvHoA8xQ2uiay263piw/exec", {
+      method: "POST",
+      body: params,
+    })
+      .then(response => response.json())
+      .then(data => alert(data.status === "success" ? data.message : `Error: ${data.message}`))
+      .catch(error => {
+        alert("Error submitting the form. Please try again.");
+        console.error(error);
+      });
+  });
